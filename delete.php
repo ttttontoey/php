02bbc0f4e2426a4fc866d4887c89f670990fc71b
ttt.tontoey <0928934814b@gmail.com>
2024-10-13 22:33:42 +0700

    upja
    upja

--- delete.php
+++ delete.php
@@ -1,22 +1,5 @@
 <?php
-<<<<<<< Updated upstream
-session_start();
-    if (isset($_SESSION['role']) && $_SESSION ['role']=='a'){
-    $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-    $sql="DELETE FROM post WHERE id=$_GET[id]";
-    $conn->exec($sql);
-    $sql="DELETE FROM comment WHERE post_id=$_GET[id]";
-    $conn->exec($sql);
-    $conn=null;
-    header("location:index.php");
-    die();
-    }else{
-    header("location:index.php");
-    die();
-    }
-?>
 
-=======
     session_start();
     if (isset($_SESSION['role']) && $_SESSION['role'] == 'a') {
 
@@ -34,4 +17,3 @@ session_start();
         die();
     }
 ?>
->>>>>>> Stashed changes
--- register_save.php
+++ register_save.php
@@ -1,27 +1,4 @@
 <?php
-<<<<<<< Updated upstream
-session_start();
-$login = $_POST['login'];
-$password = sha1($_POST['pwd']);
-$name = $_POST['name'];
-$gender = $_POST['gender'];
-$email = $_POST['email'];
-
-$conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-$sql = "SELECT * FROM user where login='$login'";
-$result = $conn->query($sql);
-if($result->rowCount()==1){
-    $_SESSION['add_login']="error";
-}else{
-    $sql1="INSERT INTO user (login,password,name,gender,email,role)
-    VALUES ('$login','$password','$name','$gender','$email','m')";
-    $conn->exec($sql1);
-    $_SESSION['add_login']="success";
-}
-$conn = null;
-header("location:register.php");
-die();
-=======
     session_start();
     $login = $_POST['login'];
     $passwd = sha1($_POST['pwd']);
@@ -46,4 +23,3 @@ die();
     header('location:register.php');
     die();
 ?>
->>>>>>> Stashed changes
